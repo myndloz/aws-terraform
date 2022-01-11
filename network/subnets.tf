b5da9ca787cc67087a6ae5e270b62e2c10ef36b5
myndloz <ndlovumpara@gmail.com>
2022-01-11 09:53:55 +0200

    Priv & Pub Subs -FINE

--- network/subnets.tf
+++ network/subnets.tf
@@ -12,4 +12,14 @@ resource "aws_subnet" "public_grtz_subnet" {
   }
 }
 
-
+resource "aws_subnet" "private_grtz_subnet" {
+  count = length(var.private_cidrs)
+  vpc_id     = aws_vpc.vpc_grtz.id
+  cidr_block = var.private_cidrs[count.index]
+  map_public_ip_on_launch = false
+  availability_zone = ["us-west-2a", "us-west-2b", "us-west-2c", "us-west-2d"][count.index]
+ 
+  tags = {
+    Name = "grtz_private_${count.index + 1}"
+  }
+}
